b136e311afe82ce8478477bfba9a959fd12ced64
Iliya Miralavy <iliyaalavy@gmail.com>
2021-05-01 19:57:41 -0400

    added some db features

--- eletility.py
+++ eletility.py
@@ -9,7 +9,7 @@ class DB:
         pass
 
     def connect(self, db_file):
-        """ create a database connection to a SQLite database """
+        """ connects to or creates a database connection to a SQLite database """
         conn = None
         try:
             conn = sqlite3.connect(db_file)
@@ -21,11 +21,21 @@ class DB:
         return True
     
     def tables(self):
+        """ returns a list containing all the table names """
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        return self.cursor.fetchall()
+        return self.cursor.fetchall()[0]
+
+    def tableExists(self, key):
+        """ returns true if the given table name exists"""
+        tables = self.tables()
+        for name in tables:
+            if name == key:
+                return True
+        return False
     
     def createTable(self, name, conf):
-        cmd = "CREATE TABLE {}(".format(name)
+        """ creates a table """
+        cmd = "CREATE TABLE IF NOT EXISTS {}(".format(name)
         for key, value in conf.items():
             cmd += "{} {}, ".format(key, value)
         cmd = cmd[:len(cmd)-2]
@@ -33,12 +43,23 @@ class DB:
         self.cursor.execute(cmd)
         self.conn.commit()
 
-    def insert(self, table, values):
-        list = "("
-        for value in values:
-            list += "`{}`, "
-        list = list[:len(list)-2] + ")"
-        cmd = "INSERT INTO {} VALUES{}".format(table, list)
+    def insertUnique(self, table, condition, values):
+        """ inserts a row to a table if a condition is met """
+        cmd = "INSERT INTO {} VALUES {} WHERE NOT EXISTS(SELECT 1 FROM memos WHERE id=5 AND text='text to insert')
+"
+        
+       
+    def insert(self, table, values, condition=False):
+        """ inserts a row to a table """
+        keys = "("
+        vals = "("
+        for key in values:
+            vals += "`{}`, ".format(values[key])
+            keys += "{}, ".format(key)
+        vals = vals[:len(vals)-2] + ")"
+        keys = keys[:len(keys)-2] + ")"
+        cmd = "INSERT INTO {} {} VALUES {}".format(table, keys, vals)
+        if not condition
         self.cursor.execute(cmd)
         self.conn.commit()
 

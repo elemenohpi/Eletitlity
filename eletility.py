0ae79724252cba95d8aaa55e4c6db837878703ff
Iliya <32390446+elemenohpi@users.noreply.github.com>
2021-05-07 22:43:08 -0400

    parser

--- eletility.py
+++ eletility.py
@@ -301,3 +301,39 @@ class Times:
             return "12"
         else:
             raise "invalid month"
+
+########################## ConfigParser ##########################
+
+class ConfigParser:
+    """ Config parser constructor """
+    def __init__(self):
+        pass
+    
+    """ reads from a given config file and returns a list of the arguements """
+    def read(self, path):
+        config = {}
+        try:
+            file = open(path, 'r')
+        except IOError as e:
+            raise Exception(e.strerror)
+        
+        lines = file.readlines()
+        for num, line in enumerate(lines):
+            line = line.split("#")[0].strip()
+            
+            if len(line) == 0:
+                # empty line, ignore
+                continue
+            elif line[0] == "#":
+                # comment
+                continue
+            else:
+                tokens = line.split("=")
+                if len(tokens) != 2:
+                    # illegal length
+                    raise Exception("Illegal format on line " + repr(num) + " of " + file)
+                else:
+                    # legal format
+                    config[tokens[0].strip()] = tokens[1].strip()
+        
+        return config
